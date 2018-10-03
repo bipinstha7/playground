aeeadfba03e89a8576db8950e4deb5c716856a33
bipinstha7 <bipin.shrestha901@gmail.com>
2018-10-04 01:40:49 +0545

    codes to create pdf in server and download in client side

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<button class="btn btn-primary" @click="getPdf">Download PDF</button>
+	</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+	methods: {
+		getPdf() {
+			console.log('hello')
+			axios.post('http://localhost:3000/pdfcsv', { responseType: 'arraybuffer' }).then(response => {
+
+				console.log('response', response.data)
+
+				const blob = new Blob([response.data], {
+					type: "application/pdf"
+				})
+				const url = window.URL.createObjectURL(blob)
+				
+
+				let csvName =
+					'Report Of The Daily Operating Time '
+				csvName = csvName.split(' ').join('_')
+
+				const a = document.createElement('a')
+				a.setAttribute('hidden', '')
+				a.setAttribute('href', response.data)
+				a.setAttribute('download', `${csvName}.pdf`)
+				document.body.appendChild(a)
+				a.click()
+				document.body.removeChild(a)
+			})
+		}
+	}
+}
+</script>
+
