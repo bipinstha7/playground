fff0eb9cb77030b97d6f08ed45c413c985ffb83e
bipinstha7 <bipin.shrestha901@gmail.com>
2018-10-04 01:40:20 +0545

    add server code

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+app.post('/pdfcsv', (req, res) => {
+    // fonts = {
+    //     Roboto: {
+    //         normal: './fonts/Roboto-Regular.ttf',
+    //         bold: './fonts/Roboto-Medium.ttf',
+    //         italics: './fonts/Roboto-Italic.ttf',
+    //         bolditalics: './fonts/Roboto-Bold.ttf'
+    //     }
+    // };
+
+
+    // var PdfPrinter = require('pdfmake/src/printer');
+    // var printer = new PdfPrinter(fonts);
+    // var fs = require('fs');
+
+
+    // var fs = require('fs');
+
+
+    // var dd = {
+    //     content: [
+    //         'First paragraph',
+    //         'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+    //     ]
+        
+    // }
+
+    // var pdfDoc = printer.createPdfKitDocument(dd);
+    // pdfDoc.pipe(fs.createWriteStream('OnlinePrint.pdf')).on('finish',function(){
+    //     console.log("success");
+    // });
+    // pdfDoc.end();
+
+    function createPdfBinary(pdfDoc, callback) {
+
+        var fontDescriptors = {
+            Roboto: {
+                normal: './fonts/Roboto-Regular.ttf',
+                bold: './fonts/Roboto-Medium.ttf',
+                italics: './fonts/Roboto-Italic.ttf',
+                bolditalics: './fonts/Roboto-Bold.ttf'
+            }
+        };
+        var pdfMakePrinter = require('pdfmake/src/printer');
+        var printer = new pdfMakePrinter(fontDescriptors);
+        
+        var doc = printer.createPdfKitDocument(pdfDoc);
+        
+        var chunks = [];
+        var result;
+        
+        doc.on('data', function (chunk) {
+            chunks.push(chunk);
+        });
+        doc.on('end', function () {
+            result = Buffer.concat(chunks);
+            callback('data:application/pdf;base64,' + result.toString('base64'));
+        });
+        doc.end();
+        
+        }
+        
+        var dd = {
+            content: [
+                'First paragraph',
+                'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+            ]
+            
+        }
+        
+        
+        createPdfBinary(dd, function(binary) {
+            res.contentType('application/pdf');
+            res.send(binary);
+        }, function(error) {
+            res.send('ERROR:' + error);
+        });
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
