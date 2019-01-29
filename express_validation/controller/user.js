281de4de427a9952e1bdaa993d0bb615bf259dc5
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-29 22:39:26 +0545

    express validation

---
+++ express_validation/controller/user.js
@@ -0,0 +1,49 @@
+const User = require('./models/user')
+const { body } = require('express-validator/check')
+
+exports.validate = method => {
+	switch (method) {
+		case 'createUser': {
+			return [
+				body('userName', "userName doesn't exists").exists(),
+				body('email', 'Invalid email')
+					.exists()
+					.isEmail(),
+				body('phone')
+					.optional()
+					.isInt(),
+				body('status')
+					.optional()
+					.isIn(['enabled', 'disabled'])
+			]
+		}
+	}
+}
+
+const validationHandler = next => result => {
+    if (result.isEmpty()) return
+
+    if (!next) {
+        throw new Error(
+            result.array.map(i => `'${i.param}' has ${i.msg}`).join(' ')
+        )
+    } else {
+        return next(result.array.map(i => `'${i.param}' has ${i.msg}`).join(' '))
+    }
+}
+
+exports.createUser = (req, res, next) => {
+	req.getValidationResult() // to get the result of above validate fn
+		.then(validationHandler())
+		.then(() => {
+			const { userName, email, phone, status } = req.body
+
+			User.create({
+				userName,
+				email,
+				phone,
+				status
+			}).then(user => res.josn(user))
+		})
+		.catch(next)
+}
---
+++ express_validation/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+
+const app = express()
+
+const router = express.Router()
+
+app.use(bodyParser.json())
+app.use(expressValidator())
+app.use('/api', router)
+
