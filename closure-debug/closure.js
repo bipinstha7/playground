8e78a14e9fcce07cb6ac1a7bcb345d1ded580131
bipinstha7 <bipin.shrestha901@gmail.com>
2019-04-26 20:08:28 +0545

    debug with function expression

--- closure-debug/closure.js
+++ closure-debug/closure.js
@@ -1,13 +1,27 @@
-function createCounter() {
-    let counter = 0
-    function myFunction() {
-      counter = counter + 1
-      return counter
-    }
-    return myFunction
+// function createCounter() {
+//     let counter = 0
+//     function myFunction() {
+//       counter = counter + 1
+//       return counter
+//     }
+//     return myFunction
+//   }
+//   const increment = createCounter()
+//   const c1 = increment()
+//   const c2 = increment()
+//   const c3 = increment()
+//   console.log('example increment', c1, c2, c3)
+
+let createCounter = function() {
+  let counter = 0
+  function myFunction() {
+    counter = counter + 1
+    return counter
   }
-  const increment = createCounter()
-  const c1 = increment()
-  const c2 = increment()
-  const c3 = increment()
-  console.log('example increment', c1, c2, c3)
+  return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3)
