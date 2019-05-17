8ca87de658930c2f9ef8a46458077e2fb45fa3ab
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-17 20:05:24 +0545

    user model with name and password fields

---
+++ jwt_node_auth/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const environment = process.env.NODE_ENV
+const stage = require('./config')[environment]
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+})
+
+module.exports = mongoose.model('User', userSchema)
