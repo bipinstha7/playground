44aac556c8a507adb18e385ed3285038d7dbead6
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-17 21:22:46 +0545

    hash password before saving the user

--- jwt_node_auth/models/users.js
+++ jwt_node_auth/models/users.js
@@ -20,4 +20,23 @@ const userSchema = new Schema({
 	},
 })
 
+userSchema.pre('save', function(next) {
+	const user = this
+
+	// don't rehash if it's an old user
+	if (!user.isModified || !user.isNew) {
+		next()
+	} else {
+		bcrypt.hash(user.password, stage.saltingRounds, (err, hash) => {
+			if (err) {
+				console.log(`Error hashing password for user ${user.name}`)
+				next(err)
+			} else {
+				user.password = hash
+				next()
+			}
+		})
+	}
+})
+
 module.exports = mongoose.model('User', userSchema)
