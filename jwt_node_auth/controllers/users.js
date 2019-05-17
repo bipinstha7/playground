537739184533e989e89a2b5b908b6b7081193d0c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-05-17 22:34:48 +0545

    login contorller to authenticate a user during login

--- jwt_node_auth/controllers/users.js
+++ jwt_node_auth/controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/users')
 
 module.exports = {
@@ -10,5 +11,27 @@ module.exports = {
 				res.status(201).send(user.name)
 			})
 			.catch(err => res.status(500).send(err))
+	},
+	login: (req, res) => {
+		const { name, password } = req.body
+
+		User.findOne({ name })
+			.then(user => {
+				if (user) {
+					bcrypt
+						.compare(password, user.password)
+						.then(match => {
+							if (match) {
+								res.status(200).send(user.name)
+							} else {
+								res.status(401).json('Authentication Error')
+							}
+						})
+						.catch(err => res.status(500).josn(err))
+				} else {
+					res.status(404).json('User Not Found')
+				}
+			})
+			.catch(err => res.status(500).json(err))
 	},
 }
